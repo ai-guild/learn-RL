079a3731f84cd4dbcdccb2f8ef0bb6f200a60804
Allen Akhaumere <allenakhaumere@gmail.com>
2021-09-08 10:30:53 +0100

    added openai

---
+++ openai_rl/pl_rl.py
@@ -0,0 +1,14 @@
+from pl_bolts.models.rl import DQN
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.loggers import TensorBoardLogger
+
+from pl_bolts.models.rl import Reinforce
+reinforce = Reinforce("CartPole-v0")
+trainer = Trainer()
+trainer.fit(reinforce)
+
+
+# ####
+# ####
+# ####
